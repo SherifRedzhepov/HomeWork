153550e5e3dae356292a7d50a950e957ffd09f64
SherifRedzhepov <iceto_8890@abv.bg>
2023-11-06 01:13:00 +0200

    initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,19 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Please enter your name:");
+
+        Scanner scanner = new Scanner(System.in);
+        String userName = scanner.nextLine();
+        System.out.println("Hello " + userName + ". Nice to meet you!");
+
+        System.out.println("What is your hobby? :");
+        String userHobby = scanner.nextLine();
+        System.out.println("Wow! "+ userHobby + " is a great hobby.");
+
+
     }
 }
